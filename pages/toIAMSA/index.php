b9e153b8e9efc174647af967bbcfbb473e7ef34d
César Valencia Campuzano <cesarvalencia@live.com.mx>
2020-01-27 19:43:28 -0600

    [index]
    Se crean las primeras paginas del desarrollo a modo de plantilla.
    La funcionalidad se ira agregando conforme avance el mismo.

---
+++ pages/toIAMSA/index.php
@@ -0,0 +1,42 @@
+
+<?php 
+
+	include_once '../../bin/generalHead.php';
+
+?>
+
+	<body>
+		
+		<header>
+<?php 
+	
+	navHead('Buscar afiliado');
+	include_once '../../bin/generalMenu.php';
+
+?>
+
+		</header>
+		
+		<main>
+		
+			<div class="section">
+				<div class="row center">
+					<h3 class="light header">Buscar por nombre</h3>
+					<div class="col s12 m6 offset-m3">
+						<p class="caption">Echa un vistazo a nuestros temas oficiales para Materialize.</p>
+						<a href="themes.html" class="btn-large waves-effect waves-light">Explora nuestros temas</a>
+					</div>
+				</div>
+			</div>
+
+		</main>
+
+<?php 
+	
+	include_once '../../bin/generalFooter.php';	
+	include_once '../../bin/generaljQuery.php';
+
+?>
+
+	</body>
+</html>
