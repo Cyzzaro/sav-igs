970064e2a9ad311b6f9463e4c3471665d87bcdbe
César Valencia Campuzano <cesarvalencia@live.com.mx>
2020-01-28 11:53:45 -0600

    [DataBase Connection]  -
    Se realizan pruebas de conexión con la Base de Datos, colocando un indicador Online/Offline en el NavHeader de la página. La conexión y los objetos requeridos para la misma, se cierran una vez es invocado el footer.

--- bin/functions/constructorNavHead.php
+++ bin/functions/constructorNavHead.php
@@ -2,14 +2,25 @@
 <?php 
 
 	function navHead($titleNav) {
+		$serverName = COBRANZASRVR;
+		$conn = sqlsrv_connect(COBRANZASRVR, array('Database' => 'tmk', 'Uid' => COBRANZAUSER, 'PWD' => COBRANZAPWD));
+
+		if($conn) {
+			echo '<span class="badge green white-text z-depth-1" data-badge-caption="Online">Online</span>';
+		} else {
+			echo '<span class="badge red white-text z-depth-1" data-badge-caption="Online">Offline</span>';
+		}
+		
 		echo '
-			<!-- BEGIN NAV OR HEAD PAGE -->
+			<!-- BEGIN TOP NAV HEAD PAGE -->
 			<nav class="top-nav">
 				<div class="container">
-					<div class="nav-wrapper"><a class="page-title">'.$titleNav.'</a></div>
+					<div class="nav-wrapper truncate">
+						<a class="page-title">'.$titleNav.'</a>
+					</div>
 				</div>
 			</nav>
-			<!-- END NAV OR HEAD PAGE -->
+			<!-- END TOP NAV HEAD PAGE -->
 			';
 	}
 
