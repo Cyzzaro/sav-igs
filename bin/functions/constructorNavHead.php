c9c9556288305de97e3e7be95b8e4d5e5765ac3e
César Valencia Campuzano <cesarvalencia@live.com.mx>
2020-01-27 19:32:48 -0600

    [bin]
    Directorio con configuraciones globales actualizado.

---
+++ bin/functions/constructorNavHead.php
@@ -0,0 +1,16 @@
+
+<?php 
+
+	function navHead($titleNav) {
+		echo '
+			<!-- BEGIN NAV OR HEAD PAGE -->
+			<nav class="top-nav">
+				<div class="container">
+					<div class="nav-wrapper"><a class="page-title">'.$titleNav.'</a></div>
+				</div>
+			</nav>
+			<!-- END NAV OR HEAD PAGE -->
+			';
+	}
+
+?>
---
+++ bin/generalPreloader.php
@@ -0,0 +1,12 @@
+
+	<!-- BEGIN PRELOADER -->
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+	<div class="progress" style="position: fixed; top: 0; left: 0; margin: 0; padding: 0; z-index: 1000;">
+		<div class="indeterminate"></div>
+	</div>
+	<script type="text/javascript">
+		$(window).load(function() {
+			$(".progress").fadeOut("slow");
+		});
+	</script>
+	<!-- END PRELOADER -->	
