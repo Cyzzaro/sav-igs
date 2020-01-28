221ce81a8d1f4629b2926d3913bca311719442d8
César Valencia Campuzano <cesarvalencia@live.com.mx>
2020-01-28 10:50:16 -0600

    [Minor updates]  -
    Prettify de material.css. Restore para Roboto Font. Introducción de material-icons. Preloader en color azul.

--- bin/generalPreloader.php
+++ bin/generalPreloader.php
@@ -2,7 +2,7 @@
 	<!-- BEGIN PRELOADER -->
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 	<div class="progress" style="position: fixed; top: 0; left: 0; margin: 0; padding: 0; z-index: 1000;">
-		<div class="indeterminate"></div>
+		<div class="indeterminate blue"></div>
 	</div>
 	<script type="text/javascript">
 		$(window).load(function() {
