247856a70ac8e2dc3dd05c8e857549473ca7e6e5
César Valencia Campuzano <cesarvalencia@live.com.mx>
2021-05-27 01:25:34 -0500

    Se retiran modulos de reporteria Apolo.

--- bin/conn/access.php
+++ bin/conn/access.php
@@ -3,7 +3,7 @@
 /**
  * Datos de acceso a BBDD de Cobranza.
  */ 
-define("COBRANZASRVR","192.168.12.116");
+define("COBRANZASRVR","localhost");
 define("COBRANZAUSER","sa");
 define("COBRANZAPWD","C354r.8908.05.2310");
 define("COBRANZABD","tmk");
