dcbd765a77a2f0599bf5f7c7e020aaac3e2c23f1
César Valencia <cesarvalencia@live.com.mx>
2022-01-17 10:17:18 -0600

    Update access.php

--- bin/conn/access.php
+++ bin/conn/access.php
@@ -3,9 +3,9 @@
 /**
  * Datos de acceso a BBDD de Cobranza.
  */ 
-define("COBRANZASRVR","localhost");
+define("COBRANZASRVR","192.168.14.60");
 define("COBRANZAUSER","sa");
-define("COBRANZAPWD","CsC00000.");
+define("COBRANZAPWD","C354r.8908.05.2310");
 define("COBRANZABD","tmk");
 
 ?>
